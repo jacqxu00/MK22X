aaae0f9b1d6a50912bb6264f5530b5210fea1e71
Jacqueline Xu <jackiestuy@gmail.com>
2017-02-14 15:29:27 -0500

    setup

---
+++ 03Knights/KnightBoard.java
@@ -0,0 +1,14 @@
+public class KnightBoard {
+
+    public KnightBoard(int startingRows,int startingCols) {}
+
+    public String toString()
+    {} //blank if you never called solve or when there is no solution
+
+    public void solve() {}
+
+    private boolean solveH(int row ,int col, int level){}
+    // level is the # of the knight
+
+
+}
