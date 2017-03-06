e55539ab3a2bb4528688e73e6fa02e3b2dcf9aa8
Jacqueline Xu <jackiestuy@gmail.com>
2017-03-06 00:21:15 -0500

    wait(20) not working but done

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -2,7 +2,10 @@ import java.util.*;
 import java.io.*;
 
 public class Maze{
-
+    private int[][] dir = {
+	{0,1,0,-1},
+	{1,0,-1,0},
+    };
     private char[][]maze;
     private boolean animate;
 
@@ -17,9 +20,71 @@ public class Maze{
       3. When the file is not found OR there is no E or S then: print an error and exit the program.
     */
     public Maze(String filename){
-        //COMPLETE CONSTRUCTOR
+	makeSize(filename);
+        loadWords(filename);
     }
 
+    private void makeSize(String fileName){
+	try {
+	    Scanner in = new Scanner(new File(fileName));
+	    int row = 0;
+	    int col = 0;
+	    while(in.hasNextLine()){
+		String line = in.nextLine();
+		col = line.length();
+		row++;
+            }
+	    maze = new char[row][col];
+	}
+	catch(FileNotFoundException e){
+	    System.out.println("Invalid filename or path");
+	    System.exit(1);
+	}
+    }
+    
+    private void loadWords(String fileName){
+	try {
+	    Scanner in = new Scanner(new File(fileName));
+	    int row = 0;
+	    int numE = 0;
+	    int numS = 0;
+	    while(in.hasNextLine()){
+		String line = in.nextLine();
+		for (int col = 0; col < line.length(); col++){
+		    char c = line.charAt(col);
+		    if (c == 'E'){
+			numE++;
+		    }
+		    if (c == 'S'){
+			numS++;
+		    }
+		    maze[row][col] = c;
+		}
+		row++;	
+            }
+	    System.out.println(toString());
+	    if (numE>1 || numS>1) {
+		System.out.println("Invalid number of starts/ends");
+		System.exit(1);
+	    }
+	}
+	catch(FileNotFoundException e){
+	    System.out.println("Invalid filename or path");
+	    System.exit(1);
+	}
+    }
+
+    public String toString() {
+	String ans = "";
+	for (int r = 0; r < maze.length; r++) {
+	    for (int c = 0; c < maze[0].length; c++) {
+		ans += maze[r][c];
+	    }
+	    ans+= "\n";
+	}
+	return ans;
+    }
+    
     public void setAnimate(boolean b){
         animate = b;
     }
@@ -34,10 +99,18 @@ public class Maze{
       Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
     */
     public boolean solve(){
-            int startr=-1,startc=-1;
-            //Initialize starting row and startint col with the location of the S. 
-            maze[startr][startc] = ' ';//erase the S, and start solving!
-            return solve(startr,startc);
+	int startr=-1,startc=-1;
+        //Initialize starting row and starting col with the location of the S. int
+        for (int r = 0; r < maze.length; r++) {
+	    for (int c = 0; c < maze[0].length; c++) {
+		if (maze[r][c] == 'S'){
+		    startr = r;
+		    startc = c;
+		}
+	    }
+	}
+	maze[startr][startc] = '@';//erase the S, and start solving!
+	return solve(startr,startc);
     }
 
     /*
@@ -56,11 +129,48 @@ public class Maze{
     private boolean solve(int row, int col){
         if(animate){
             System.out.println("\033[2J\033[1;1H"+this);
-            wait(20);
+	    //wait(20);
         }
-
-        //COMPLETE SOLVE
-        return false; //so it compiles
+	
+	for (int move = 0; move < 4; move++) {
+	    int newRow = row + dir[0][move];
+	    int newCol = col + dir[1][move];
+	    int revRow = row - dir[0][move];
+	    int revCol = col - dir[0][move];
+	    if (isValidMove(newRow,newCol)) {
+		if (maze[newRow][newCol]=='E'){
+		    return true;
+		}
+		mark(newRow,newCol);
+		if (solve(newRow,newCol)) {
+		    return true;
+		}
+		unmark(newRow,newCol);
+		//return solve(rev,col);
+	    }
+	}
+	return false;
     }
 
+    private boolean isValidMove(int r,int c) {
+	return maze[r][c]==' ' || maze[r][c]=='E';
+    }
+
+    private void mark(int row,int col) {
+	maze[row][col]='@';
+	//System.out.println("marked\n"+toString());
+    }
+
+    private void unmark(int row,int col) {
+	maze[row][col]='.';
+    }
+    
+    public static void main(String[] args) {
+        Maze f;
+        f = new Maze("data3.txt");//true animates the maze.
+        f.setAnimate(true);
+        f.solve();
+        System.out.println(f);
+    }
+    
 }
