0da224567903ee1dc07ae70df02c359d30b519f3
Jacqueline Xu <jackiestuy@gmail.com>
2017-03-16 05:36:29 -0400

    testing part more

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -4,17 +4,20 @@ public class Quick {
     
     public static int part(int[] data, int start, int end){
 	int range = (end - start) + 1;
-	int rand = (int)(Math.random() * range) + start;
+	int rand = 0; //(int)(Math.random() * range) + start;
 	int num = data[rand];
-	//System.out.println(num);
+	System.out.println(num);
 	int s = start;
 	int e = end;
 	int i = start;
+	System.out.println("\ns = "+start);
+	System.out.println("e = "+end);
 	for (int n = start; n <= end; n++) {
 	    //System.out.println("\ns = "+s);
 	    //System.out.println("e = "+e);
 	    //System.out.println("i = "+i);
 	    //System.out.println("rand = "+rand);
+	    //System.out.println("n = "+n);
 	    //System.out.println(toString(data));
 	    if (i != rand) {
 		//System.out.println(data[i]);
@@ -25,6 +28,7 @@ public class Quick {
 		    data[s] = rec;
 		    if (s == rand) {
 			rand = i;
+			n--;
 		    }
 		    if (s != i) {
 			i--;
@@ -44,7 +48,7 @@ public class Quick {
 	    }
 	    i++;
 	}
-	//System.out.println("final\n"+toString(data));
+	System.out.println("final\n"+toString(data));
 	data[s] = num;
 	return s;
     }
@@ -75,16 +79,45 @@ public class Quick {
 	return ans;
     }
 
+    public static void selectionSort(int[] data){
+	if (data.length > 1) {
+	    for (int i = 0; i < data.length - 1; i++) {
+		int smallest = data[i];
+		int smallind = i;
+		for (int j = i+1; j < data.length; j++) {
+		    if (data[j] < smallest) {
+			smallest = data[j];
+			smallind = j;
+		    }
+		}
+	   
+		if (smallest != data[i]) {
+		    data[smallind] = data[i];
+		    data[i] = smallest;
+		}
+	    }
+	}
+    }
+
     public static void main(String[] args) {
-	int[] n = {4,0,3,2,6,1,7,5,8};
-	System.out.println(part(n,0,8));
-	int[]ary = { 2, 10, 15, 23, 0,  5};
-	System.out.println(quickselect( ary , 0 )); //would return 0
+	for (int st = 0; st < 9; st++) {
+	    for (int en = st; en < 9; en++) {
+		int[] n = new int[10];
+		for (int i = 0; i < n.length; i++) {
+		    n[i] = (int)(Math.random() * 100);
+		}
+		selectionSort(n);
+		System.out.println(toString(n));
+		System.out.println(part(n,st,en));
+	    }
+	}
+	//int[]ary = { 2, 10, 15, 23, 0,  5};
+	/*System.out.println(quickselect( ary , 0 )); //would return 0
 	System.out.println(quickselect( ary , 1 ));  //would return 2
 	System.out.println(quickselect( ary , 2 ));  //would return 5
 	System.out.println(quickselect( ary , 3 ));  //would return 10
 	System.out.println(quickselect( ary , 4 ));  //would return 15
-	System.out.println(quickselect( ary , 5 ));  //would return 23
+	System.out.println(quickselect( ary , 5 ));  //would return 23*/
     }
     
 }
