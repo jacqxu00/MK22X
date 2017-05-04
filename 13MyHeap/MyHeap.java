0b2c682f2b7f9dc3b3cced3d633a56084b140e93
Jacqueline Xu <jackiestuy@gmail.com>
2017-05-04 15:30:20 -0400

    pushup pushdown done

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -25,20 +25,61 @@ public class MyHeap {
 	}
 	else {
 	    arr.add(s);
-	    //pushup
+	    pushUp();
 	}
     }
 
     public String remove() {
-	String ans = arr.remove(1);
-	//pushdown
-	return ans;
+	if (arr.size() > 1) {
+	    String ans = arr.remove(1);
+	    swap(arr.size()-1,1);
+	    pushDown;
+	    return ans;
+	}
     }
 
     private void pushUp() {
+	int index = arr.size()-1;
+	while (index/2 >= 1) {
+	    while (arr.get(index).compareTo(arr.get(index/2))*constant < 0) {
+		swap(index,index/2);
+		index = index/2;
+	    }
+	}
     }
 
     private void pushDown() {
+	int index = 1;
+	while (arr.size() >= 2*index) {
+	    if (arr.size() < 2*index+1) {
+		if (arr.get(index).compareTo(arr.get(index*2)*constant > 0)) {
+		    swap(index, index*2);
+		    index = index*2;
+		}
+	    }
+	    else {
+		while (arr.get(index).compareTo(arr.get(index*2)*constant > 0) || arr.get(index).compareTo(arr.get(index*2+1)*constant > 0)) {
+		    if (arr.get(index*2).compareTo(arr.get(index*2+1)) > 0) {
+			swap(index, index*2);
+			index = index*2;
+		    }
+		    else {
+			swap(index, index*2+1);
+			index = index*2+1;
+		    }
+		}
+	    }
+	}
+    }
+
+    private void swap(int a, int b) {
+	String temp = arr.get(a);
+	arr.add(a,arr.get(b));
+	arr.add(b,temp);
+    }
+
+    public void main(String[] args) {
+	
     }
 
 }
