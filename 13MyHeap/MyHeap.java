5efb98f50b628cd1e4d8b3adc9004ffa5d1e52da
Jacqueline Xu <jackiestuy@gmail.com>
2017-05-08 15:30:21 -0400

    fixed heap

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class MyHeap {
     private int constant;
     private ArrayList<String> arr;
-    private int size;
 
     public MyHeap() {
 	arr = new ArrayList<String>();
@@ -30,12 +29,11 @@ public class MyHeap {
 	    arr.add(s);
 	    pushUp();
 	}
-	size++;
     }
 
     public String remove() {
 	String ans = arr.get(1);
-	if (arr.size() == 1) {
+	if (arr.size() == 2) {
 	    arr.remove(1);
 	}
 	else if (arr.size() > 2) {
@@ -56,12 +54,12 @@ public class MyHeap {
 
     private void pushDown() {
 	int index = 1;
-	while (arr.size() >= 2*index) {
-	    if (arr.size() <= 2*index+1) {
+	while (arr.size()-1 >= 2*index) {
+	    if (arr.size()-1 < 2*index+1) {
 		if ((arr.get(index).compareTo(arr.get(index*2))*constant) > 0) {
 		    swap(index, index*2);
-		    index = index*2;
 		}
+		index = index*2;
 	    }
 	    else {
 		if ((arr.get(index*2).compareTo(arr.get(index*2+1))*constant) < 0) {
@@ -74,6 +72,8 @@ public class MyHeap {
 		    
 		}
 	    }
+	    //System.out.println(index);
+	    //System.out.println(toString());
 	}
     }
 
@@ -84,7 +84,7 @@ public class MyHeap {
     }
 
     public int getSize() {
-	return size;
+	return arr.size() - 1;
     }
 
     public String toString() {
@@ -99,10 +99,8 @@ public class MyHeap {
     }
     
     public static void main(String[] args) {
-	MyHeap heap = new MyHeap(true);
+	MyHeap heap = new MyHeap(false);
 	heap.add("5");
-	System.out.println(heap + "\n");
-	heap.remove();
 	System.out.println(heap + "\n");
 	heap.add("5");
 	System.out.println(heap + "\n");
